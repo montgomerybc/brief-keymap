57819ca8db10f344838628dffc76d74db6117c2c
Dave Montgomery <github@montgomerybc.ca>
2020-03-01 23:55:04 -0800

    #3 Basic unit tests for <HOME>

--- test/extension.test.ts
+++ test/extension.test.ts
@@ -1,22 +1,108 @@
-//
-// Note: This example test is leveraging the Mocha test framework.
-// Please refer to their documentation on https://mochajs.org/ for help.
-//
-
 // The module 'assert' provides assertion methods from node
 import * as assert from 'assert';
 
-// You can import and use all API from the 'vscode' module
-// as well as import your extension to test it
 import * as vscode from 'vscode';
 import * as myExtension from '../src/extension';
+import { join, resolve } from 'path';
 
-// Defines a Mocha test suite to group tests of similar kind together
-suite("Extension Tests", () => {
+const documentName = './the-mysterious-affair-at-styles-by-agatha-christie.txt';
+const testDocument = resolve(__dirname, `../../test/${documentName}`);
 
-    // Defines a Mocha unit test
-    test("Something 1", () => {
-        assert.equal(-1, [1, 2, 3].indexOf(5));
-        assert.equal(-1, [1, 2, 3].indexOf(0));
+// function move(line: number, column: number): void;
+async function move(line: number, column: number, editor: vscode.TextEditor = null) {
+    if (editor === null) {
+        editor = vscode.window.activeTextEditor;
+    }
+
+    const position = new vscode.Position(line, column);
+    editor.selection = new vscode.Selection(position, position);
+
+    await vscode.commands.executeCommand('revealLine', { lineNumber: line-5, at: 'top' });
+}
+
+suite("Home Tests", () => {
+    setup((done) => {
+        console.log("Home Tests/setup");
+
+        closeAll().then(() => {
+            vscode.workspace.openTextDocument(testDocument).then(
+                (file) => {
+                    vscode.window.showTextDocument(file).then(
+                        (editor) => {
+                            done();
+                        }
+                        , (reason) => {
+                            done(reason);
+                        }
+                    );
+                }
+            );
+        });
     });
+
+    teardown(async () => {
+        console.log("Home Tests/teardown");
+        await closeAll();
+    });
+
+    test("Pressing <HOME> once, moves to beginning of line", async () => {
+        console.log("Home Tests/Pressing <HOME> once, moves to beginning of lineup");
+
+        await move(402, 192);
+
+        await myExtension.Brief.home();
+
+        var pos = vscode.window.activeTextEditor.selection
+        assert.equal(402, pos.anchor.line);
+        assert.equal(402, pos.active.line);
+        assert.equal(0, pos.anchor.character);
+        assert.equal(0, pos.active.character);
+    });
+
+    test("Pressing <HOME> twice, moves to beginning of window", async () => {
+        console.log("Home Tests/Pressing <HOME> twice, moves to beginning of window");
+
+        await move(402, 192);
+
+        var windowPosBefore = (vscode.window.activeTextEditor as any).visibleRanges[0];
+
+        await myExtension.Brief.home();
+        await myExtension.Brief.home();
+
+        var pos = vscode.window.activeTextEditor.selection
+        assert.equal(pos.anchor.line, windowPosBefore.start.line);
+        assert.equal(pos.active.line, windowPosBefore.start.line);
+        assert.equal(pos.anchor.character, 0);
+        assert.equal(pos.active.character, 0);
+    }).timeout(10000);
+
+    test("Pressing <HOME> thrice, moves to beginning of file", async () => {
+        console.log("Home Tests/Pressing <HOME> thrice, moves to beginning of file");
+
+        await move(402, 192);
+
+        await myExtension.Brief.home();
+        await myExtension.Brief.home();
+        await myExtension.Brief.home();
+
+        var pos = vscode.window.activeTextEditor.selection
+        assert.equal(pos.anchor.line, 0);
+        assert.equal(pos.active.line, 0);
+        assert.equal(pos.anchor.character, 0);
+        assert.equal(pos.active.character, 0);
+    }).timeout(10000);
+
+    async function closeAll(): Promise<void> {
+        console.log(`Home Tests/closeAll vscode.window.visibleTextEditors.length=${vscode.window.visibleTextEditors.length}`);
+        return new Promise((c, e) => {
+            if (vscode.window.visibleTextEditors.length === 0) {
+                return c();
+            }
+            vscode.commands.executeCommand("workbench.action.revertAndCloseActiveEditor").then(() => {
+                closeAll().then(() => {
+                    c();
+                });
+            });
+        });
+    }
 });
